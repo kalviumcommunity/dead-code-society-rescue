2cdf34108f195f72014ec1fc5ee8ac76db9296f4
Pavithra8805 <pavithra8805@gmail.com>
2026-05-05 22:50:10 +0530

    refactor: replace var with const/let, rewrite promises as async/await

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-var Schema = mongoose.Schema;
+const { Schema } = mongoose;
 
-var userSchema = new Schema({
+const userSchema = new Schema({
     name: {
         type: String,
         required: true

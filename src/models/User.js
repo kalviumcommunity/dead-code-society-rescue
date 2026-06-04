cbd8f2aff5e4462058b90769123cda961384b494
RISHIB78 <rishibhateja700@gmail.com>
2026-06-04 11:23:19 +0530

    security: replace MD5 with bcrypt, extract auth middleware

--- src/models/User.js
+++ src/models/User.js
@@ -13,7 +13,6 @@ const userSchema = new Schema({
         unique: true
     },
     password: {
-        // SMELL: [CRITICAL] Password uses MD5 which is insecure. Should be hashed with bcrypt.
         type: String, // using md5 for now, easy to test
         required: true
     },
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,13 +1,12 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
 const JWT_SECRET = process.env.JWT_SECRET || 'secret123';
 
 const register = async (userData) => {
     const data = { ...userData };
-    
-    // SMELL: [CRITICAL] MD5 is a hash function, not a password hashing algorithm. Use bcrypt with 12 rounds.
-    data.password = md5(data.password);
+    const saltRounds = 12;
+    data.password = await bcrypt.hash(data.password, saltRounds);
     const newUser = new User(data);
     return await newUser.save();
 };
@@ -17,7 +16,9 @@ const login = async (email, password) => {
     if (!user) {
         return { error: 'No user found with that email' };
     }
-    if (user.password === md5(password)) {
+    
+    const isValid = await bcrypt.compare(password, user.password);
+    if (isValid) {
         const token = jwt.sign(
             { id: user._id, role: user.role }, 
             JWT_SECRET, 
